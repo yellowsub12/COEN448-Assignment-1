d700b7cacd21c23fad6ae41d56585728bd8004e2
yellowsub12 <fantasm1337@gmail.com>
2023-02-16 17:18:25 -0500

    General methods tested

--- coen6717/A1/queue/QueueJUnitTest.java
+++ coen6717/A1/queue/QueueJUnitTest.java
@@ -12,24 +12,24 @@ class QueueJUnitTest {
 	private ADTQueue<Integer> Q2;
 	private ADTQueue<Integer> Q3;
 
-	/** Clear() : Reinitialize the queue.  The user is responsible for
+	/* Clear() : Reinitialize the queue.  The user is responsible for
 	 reclaiming the storage used by the queue elements. */
 
-	/** Enqueue() : Place an element at the rear of the queue.
+	/* Enqueue() : Place an element at the rear of the queue.
  	@param it The element being enqueued. */
 
-	/** Dequeue() : dequeue() and return element at the front of the queue.
+	/* Dequeue() : dequeue() and return element at the front of the queue.
  	@return The element at the front of the queue. */
 
-	/** frontValue() : @return The front element. */
+	/* frontValue() : @return The front element. */
 
-	/** length() : @return The number of elements in the queue. */
+	/* length() : @return The number of elements in the queue. */
 	
 
 	@BeforeEach
 	void setUp() throws Exception {
 		// LQueue has front, rear, size
-		// LQueue has methods init(), clear(), enqueue(), dequeue(), frontValue(), length(), toString()
+		// LQueue has methods clear(), enqueue(), dequeue(), frontValue(), length(), toString()
 		Q1 = new LQueue<Integer>();
 
 		// AQueue has default size of 10, maxSize, front, rear, listArray. One extra space is allocated at creation.
@@ -37,29 +37,42 @@ class QueueJUnitTest {
 		Q2 = new AQueue<Integer>(15);
 
 		// DQueue has front, rear, size
-		// DQueue methods include enqueue, dequeue, frontValue (not completed), length, toString,
+		// DQueue has methods enqueue(), dequeue(), frontValue (not completed), length(), toString(),
 		Q3 = new DQueue<Integer>();
 		
 		
 	}
 
+	// Test 0
+	// Tester : Ali Turkman
+	// Date : February 16th 2022
+	// Test input data : < 15 10 10 20 15 10 20 >
+	// Test type : functional, blackbox
+	// Description : All methods of the L Queue are tested, including enqueue(), dequeue(), clear(), frontValue(), length(), and toString().
+	// Expected output < empty queue, 10, 2, 10 20 15 10 20, empty queue, 10 20 15 10 20 >
 	@Test
-	public void testGeneral() {
+	public void testGeneralLQueue() {
 		int temp;
+		Q1.enqueue(15);
+		Q1.enqueue(10);
+		Q1.clear();
+		assertEquals("< >", Q1.toString());
 		
-		Q2.enqueue(10);
-		Q2.enqueue(20);
-		Q2.enqueue(15);
-		Q2.enqueue(10);
-		Q2.enqueue(20);
-		assertEquals("< 10 20 15 10 20 >", Q2.toString());
-		
-		while(Q2.length() > 0) {
-		  temp = Q2.dequeue();
-		  Q1.enqueue(temp);
-		}
-		assertEquals("< >", Q2.toString());
+		Q1.enqueue(10);
+		assertEquals(10, Q1.frontValue());
+		Q1.enqueue(20);
+		assertEquals(2, Q1.length());
+		Q1.enqueue(15);
+		Q1.enqueue(10);
+		Q1.enqueue(20);
 		assertEquals("< 10 20 15 10 20 >", Q1.toString());
+		
+		while(Q1.length() > 0) {
+		  temp = Q1.dequeue();
+		  Q2.enqueue(temp);
+		}
+		assertEquals("< >", Q1.toString());
+		assertEquals("< 10 20 15 10 20 >", Q2.toString());
 
 	}
 
